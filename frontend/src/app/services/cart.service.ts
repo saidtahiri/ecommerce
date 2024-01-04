e2fe6949158ea9f92b19c1c5e423f17ec56ec399
Said TAHIRI <said.tahiri@hotmail.com>
2024-01-04 22:57:28 +0100

    update

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -5,6 +5,8 @@ import { OrderService } from './order.service';
 import { cartModelPublic, cartModelServer } from '../models/cart.model';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import { json } from 'stream/consumers';
+import { ProductModelServer } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -33,15 +35,38 @@ cartDataObservable$ = new BehaviorSubject<cartModelServer>(this.cartDataServer)
 
 
 
-  constructor(private http:HttpClient,
-    productService:ProductService,
-     private orderService:OrderService,
+  constructor(private http:HttpClient,private productService:ProductService,private orderService:OrderService,
      private router:Router) {
       this.cartTotal$.next(this.cartDataServer.total);
       this.cartDataObservable$.next(this.cartDataServer);
 
+     let info = JSON.parse(localStorage.getItem('cart')+'');
+      if(info ===! null && info===! undefined && info.prodData[0].incart ===!0 ){
+        //this means that my localStorage is not empty. and has some information
+        this.cartDataClient = info;
+
+        this,this.cartDataClient.prodData.forEach(p=>{
+          this.productService.getSingleProduct(p.id).subscribe((actuelProductInfo:ProductModelServer)=>{
+            if(this.cartDataServer.data[0].numInCart===0 ){
+              this.cartDataServer.data[0].numInCart = p.incart;
+              this.cartDataServer.data[0].product =actuelProductInfo;
+            }
+          });
+        });
+
+      }
+
+
+
 
    }
+
+  //Get infos from localstorage
+
+  
+
+  //check if thr variabl is null or has data on it
+
 
 
 
