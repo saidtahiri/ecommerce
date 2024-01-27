1ba29ef989ada777a3e83a853356d9f0de9efdf4
Tahiri Said <said.tahiri@hotmail.com>
2024-01-27 13:08:25 +0100

    changes

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -4,11 +4,12 @@ import Swal from 'sweetalert2';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { cartModelServer } from '../../models/cart.model';
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CurrencyPipe,RouterLink,RouterLinkActive,CommonModule],
+  imports: [CurrencyPipe,RouterLink,RouterLinkActive,CommonModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   providers:[CartService]
@@ -17,14 +18,13 @@ export class HeaderComponent implements OnInit {
 
   cartData!: cartModelServer;
   cartTotal!:number;
-  constructor(protected carteService :CartService){
+  constructor(protected carteService :CartService,protected router:Router){
 
   }
 
   ngOnInit(): void {
     this.carteService.cartTotal$.subscribe(total =>this.cartTotal= total);
     this.carteService.cartDataObservable$.subscribe(data =>this.cartData = data)
-    
   }
   
   clickme(){
@@ -33,5 +33,10 @@ export class HeaderComponent implements OnInit {
       text: 'Your data has been saved.',
       icon: 'success'
     });
+  }
+
+
+  selectProduct(id: number) {
+    this.router.navigate(['/products', id]).then();
   }
 }
