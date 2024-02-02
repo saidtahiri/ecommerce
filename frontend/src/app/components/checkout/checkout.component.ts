c908d5de1279f76bfa9c2e460f3f05443f6c03d6
Tahiri Said <said.tahiri@hotmail.com>
2024-02-02 15:27:05 +0100

    update checkout function

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
   import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
   import { Component, OnInit } from '@angular/core';
-  
+  import Swal from 'sweetalert2';
   import { cartModelServer } from '../../models/cart.model';
   import { CartService } from '../../services/cart.service';
   import { OrderService } from '../../services/order.service';
@@ -25,12 +25,40 @@ import { Router } from '@angular/router';
     ngOnInit(): void {
       this.carteService.cartDataObservable$.subscribe(data => this.cartData = data);
       this.carteService.cartTotal$.subscribe(total => this.cartTotal= total);
-      console.log(this.cartData);
+      
     }
 
     onCheckout() {
-      
-      }
+      Swal.fire({
+        title: "Placing your order, please wait...", // Customize the loading message
+        allowOutsideClick: false, // Prevent clicking outside the modal to close
+        willOpen: () => {
+          
+          // Set showConfirmButton to false during loading
+          Swal.update({ showConfirmButton: false });
+          Swal.showLoading();
+        },
+      });
+    
+      this.carteService.checkoutFromCart(1);
+      // Simulate an asynchronous operation (replace this with your actual async logic)
+      setTimeout(() => {
+        // Close the Swal.fire modal when the async operation is complete
+        Swal.close();
+    
+        // Display your actual Swal.fire dialog with the "OK" button
+        Swal.fire({
+          title: "Operation Complete",
+          text: "Your order has been placed successfully!",
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+          showConfirmButton: true, // Show the "OK" button
+        });
+      }, 2000);
+    }
+    
+    
+    
 
 
   }
