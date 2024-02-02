e2e4b440705aece5c98b48873082639f98d634dd
Tahiri Said <said.tahiri@hotmail.com>
2024-02-02 18:09:58 +0100

    updated the logic of thanx page

--- frontend/src/app/components/thankyou/thankyou.component.ts
+++ frontend/src/app/components/thankyou/thankyou.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-thankyou',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CurrencyPipe],
   templateUrl: './thankyou.component.html',
   styleUrl: './thankyou.component.css'
 })
-export class ThankyouComponent {
+export class ThankyouComponent implements OnInit {
+
+  message!:string;
+  orderId!:number;
+  cartTotal!:number;
+  products:[]=[];
+
+  constructor(private router:Router,orderService:OrderService){
+
+  }
+
+
+  ngOnInit(): void {
+    
+  }
+
+
 
 }
