34210ddd05f887f012652e25cd581e052f3b3f97
Tahiri Said <said.tahiri@hotmail.com>
2024-02-03 01:56:02 +0100

    changes

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,51 +1,44 @@
-import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
-import { CurrencyPipe } from '@angular/common';
-import Swal from 'sweetalert2';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
-import {MatPaginatorModule} from '@angular/material/paginator';
-
-
-
+import { MatPaginator } from '@angular/material/paginator';
+import { CurrencyPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [NgFor, CurrencyPipe, RouterLink, RouterLinkActive, HttpClientModule, NgxSpinnerModule, ToastrModule,MatPaginatorModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  standalone :true,
+  styleUrls: ['./home.component.css'],
+  imports:[MatPaginator,NgFor,CurrencyPipe]
 })
 export class HomeComponent implements OnInit {
   products: any = [];
-  constructor(private cartService: CartService,
-     private productService: ProductService,
-      private router: Router) {
+  pageSize = 10; // Set your desired page size
+  currentPage = 1; // Set the initial page
 
-  }
+  constructor(private cartService: CartService, private productService: ProductService, private router: Router) {}
+
   ngOnInit() {
-    this.productService.getAllProducts().subscribe((prods) => {
-      this.products = prods;
-      //console.log(this.products)
-    
+    this.loadProducts();
+  }
 
+  loadProducts() {
+    this.productService.getAllProducts(this.currentPage, this.pageSize).subscribe((prods) => {
+      this.products = prods;
     });
   }
+
   selectProduct(id: number) {
     this.router.navigate(['/products', id]).then();
   }
-  addedToCart(id: number) {
-    
-    this.cartService.addProductToCard(id);
 
+  addedToCart(id: number) {
+    this.cartService.addProductToCard(id);
   }
 
-
+  onPageChange(event: any) {
+    this.currentPage = event.pageIndex + 1;
+    this.loadProducts();
+  }
 }
-
-
