949b1fc4c0e65812eee3c6d5be094c1c603067a2
Tahiri Said <said.tahiri@hotmail.com>
2024-02-03 01:56:15 +0100

    changes

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductModelServer, ServerResponse } from '../models/product.model';
@@ -9,31 +9,32 @@ import { ProductModelServer, ServerResponse } from '../models/product.model';
 export class ProductService {
 
   SERVER_URL = "http://localhost:3000/api";
-  constructor(private http: HttpClient) { 
-    //this is my constructor
-    
-  }
+
+  constructor(private http: HttpClient) {}
+
+// product.service.ts
+getAllProducts(page: number = 1, limit: number = 12): Observable<ServerResponse> {
+  const params = new HttpParams()
+    .set('page', page.toString())
+    .set('limit', limit.toString());
+
+  return this.http.get<ServerResponse>(this.SERVER_URL + "/products", { params });
+}
 
 
-  //Get All Products
-  getAllProducts(numberOfResults = 12): Observable<ServerResponse> {
-
-    return this.http.get<ServerResponse>(this.SERVER_URL + "/products", {
-      params: { limit: numberOfResults.toString() }
-    });
-  }
-
- 
-
-  //Get Single Product From the Server
+  // Get Single Product From the Server
   getSingleProduct(id: Number): Observable<ProductModelServer> {
-    /* debugger; */
-    return  this.http.get<ProductModelServer>(this.SERVER_URL + "/products/" + id);
+    return this.http.get<ProductModelServer>(this.SERVER_URL + "/products/" + id);
   }
 
-  //Get Products From One Category
+  // Get Products From One Category
   getProductsByCategory(categoryName: string): Observable<ProductModelServer[]> {
     return this.http.get<ProductModelServer[]>(this.SERVER_URL + "/products/category/" + categoryName);
-
   }
 }
+
+
+
+
+
+
